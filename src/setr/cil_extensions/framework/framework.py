7a68a6e38c15d9188ad16a1639f98db9982010e5
Sam Porter <sam.porter.18@ucl.ac.uk>
2025-07-22 16:48:19 +0100

    UPDATE some utility functions

--- src/setr/cil_extensions/framework/framework.py
+++ src/setr/cil_extensions/framework/framework.py
@@ -6,3 +6,7 @@ class EnhancedBlockDataContainer(BlockDataContainer):
     def max(self) -> float:
         """Return the maximum value across all containers."""
         return max(d.max() for d in self.containers)
+    
+    def get_uniform_copy(self, n) -> "EnhancedBlockDataContainer":
+        """Return a copy with each container filled with n."""
+        return EnhancedBlockDataContainer(*[x.clone().fill(n) for x in self.containers])
